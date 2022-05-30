3ab8098c91299df902f7b07d455a8b0b7f7c4d33
AadOfzo <aadofzo@gmail.com>
2022-05-30 21:25:32 +0200

    eerste commit

---
+++ src/Translator.java
@@ -0,0 +1,20 @@
+package src;
+
+import java.util.HashMap;
+
+public class Translator {
+    public static void main (String[] args) {
+    HashMap<Integer,String> numericAlpha = new HashMap<String,String>();
+
+        numericAlpha.put ("Een");
+        numericAlpha.put ("Twee");
+        numericAlpha.put ("Drie");
+        numericAlpha.put ("Vier");
+        numericAlpha.put ("Vijf");
+        numericAlpha.put ("Zes");
+        numericAlpha.put ("Zeven");
+        numericAlpha.put ("Acht");
+        numericAlpha.put ("Negen");
+        numericAlpha.put ("Nul");*/
+    }
+}
